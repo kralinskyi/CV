a190afbcdfba645b9d0c349bcb25cbe3fe0e8d5f
Kralinskyi Ivan <i.kralinski@gmail.com>
2024-05-21 13:37:43 +0300

    create new branch

--- schemas/usersSchemas.js
+++ schemas/usersSchemas.js
@@ -5,9 +5,13 @@ import { emailRegepxp } from "../constants/user-constants.js";
 export const userSignupSchema = Joi.object({
   email: Joi.string().pattern(emailRegepxp).required(),
   password: Joi.string().min(6).required(),
+}).error((err) => {
+  return new Error("Помилка від Joi або іншої бібліотеки валідації");
 });
 
 export const userSigninSchema = Joi.object({
   email: Joi.string().pattern(emailRegepxp).required(),
   password: Joi.string().min(6).required(),
+}).error((err) => {
+  return new Error("Помилка від Joi або іншої бібліотеки валідації");
 });
